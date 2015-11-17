95accb87e777a5ca8d25326eb5a147907d3b13d3
Kathy20 <Kathy.20@hotmail.es>
2015-11-16 23:05:09 -0600

    Final Interface

--- windowInitial.c
+++ windowInitial.c
@@ -21,13 +21,25 @@ void on_window_destroy (GtkWidget *object, gpointer user_data) {
 int main(int argc, char *argv[])
 {
 	GtkWidget *btnLogin;
+	GdkColor color;
+    color.red = 0000;
+    color.green = 0000;
+    color.blue = 0;
+
+    GdkColor color2;
+    color2.red = 0xfff;
+    color2.green = 0xffff;
+    color2.blue = 0xfff;
 	gtk_init(&argc, &argv);
 	gtkBuilder = gtk_builder_new();
 	gtk_builder_add_from_file(gtkBuilder, "windowInitial.glade", NULL);
 	window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "windowInitial"));
-	gtk_widget_set_size_request(GTK_WINDOW(window),900,800);
+	gtk_widget_set_size_request(GTK_WINDOW(window),500,400);
+	gtk_widget_modify_bg(GTK_WINDOW(window), GTK_STATE_NORMAL, &color);
+
 	btnLogin = GTK_WIDGET (gtk_builder_get_object (gtkBuilder, "btnLogin"));
 	txtUser= GTK_WIDGET (gtk_builder_get_object (gtkBuilder, "txtUser"));
+	gtk_widget_modify_bg(GTK_WIDGET(txtUser),GTK_STATE_NORMAL, &color2);
 	txtPassword=GTK_WIDGET (gtk_builder_get_object (gtkBuilder, "txtPassword"));
 	g_signal_connect (window, "destroy", G_CALLBACK (on_window_destroy), NULL);
 	gtk_widget_set_tooltip_text (btnLogin, "Proporcione los datos indicados para realizar el inicio de sesión. ");
